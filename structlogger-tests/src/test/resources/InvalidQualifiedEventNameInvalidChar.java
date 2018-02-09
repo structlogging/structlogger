c04c6559ad295f90304681be8f06a51820f4fdfd
Tantalor93 <Obenky@gmail.com>
2018-02-09 14:26:32 +0100

    cover more statement level compilation errors with tests

---
+++ structlogger-tests/src/test/resources/InvalidQualifiedEventNameInvalidChar.java
@@ -0,0 +1,21 @@
+import cz.muni.fi.Slf4jLoggingCallback;
+import cz.muni.fi.annotation.LoggerContext;
+import cz.muni.fi.EventLogger;
+import cz.muni.fi.DefaultContext;
+
+public class InvalidQualifiedEventNameInvalidChar {
+
+    @LoggerContext(context = DefaultContext.class)
+    private static EventLogger<DefaultContext> defaultLog = new EventLogger<>(
+            new Slf4jLoggingCallback(
+                    LoggerFactory.getLogger("cz.muni.fi.Default")
+            )
+    );
+
+    public void test() {
+        defaultLog.info("Should not compile {} {}")
+                .varDouble(1.2)
+                .varBoolean(false)
+                .log("some.different.\"ShouldNotBeCreated");
+    }
+}
