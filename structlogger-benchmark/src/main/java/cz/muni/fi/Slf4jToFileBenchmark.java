5b4a31b26c445a32608cd370c663f358bab1fca8
Tantalor93 <Obenky@gmail.com>
2018-02-09 09:52:06 +0100

    improve benchmarks

--- structlogger-benchmark/src/main/java/cz/muni/fi/Slf4jToFileBenchmark.java
+++ structlogger-benchmark/src/main/java/cz/muni/fi/Slf4jToFileBenchmark.java
@@ -32,12 +32,7 @@
 package cz.muni.fi;
 
 import cz.muni.fi.annotation.LoggerContext;
-import org.openjdk.jmh.annotations.Benchmark;
-import org.openjdk.jmh.annotations.BenchmarkMode;
-import org.openjdk.jmh.annotations.Measurement;
-import org.openjdk.jmh.annotations.Mode;
-import org.openjdk.jmh.annotations.OutputTimeUnit;
-import org.openjdk.jmh.annotations.Warmup;
+import org.openjdk.jmh.annotations.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -45,6 +40,7 @@ import java.util.concurrent.TimeUnit;
 
 @BenchmarkMode(Mode.AverageTime)
 @OutputTimeUnit(TimeUnit.NANOSECONDS)
+@Fork(5)
 public class Slf4jToFileBenchmark {
 
     @LoggerContext(context = DefaultContext.class)
@@ -80,30 +76,72 @@ public class Slf4jToFileBenchmark {
     @Warmup(iterations = 5)
     @Measurement(iterations = 5)
     @Benchmark
-    public void structLogger8CallsWithParametrizedMessage() {
-        structLogger8CallsWithParametrizedMessage(1);
+    public void structLogger10CallsWithParametrizedMessage() {
+        structLogger10CallsWithParametrizedMessage(1);
     }
 
     @Warmup(iterations = 5)
     @Measurement(iterations = 5)
     @Benchmark
-    public void structLogger16CallsWithParametrizedMessage() {
-        structLogger8CallsWithParametrizedMessage(2);
+    public void structLogger20CallsWithParametrizedMessage() {
+        structLogger10CallsWithParametrizedMessage(2);
+    }
+
+    @Warmup(iterations = 5)
+    @Measurement(iterations = 5)
+    @Benchmark
+    public void structLogger30CallsWithParametrizedMessage() {
+        structLogger10CallsWithParametrizedMessage(3);
+    }
+
+    @Warmup(iterations = 5)
+    @Measurement(iterations = 5)
+    @Benchmark
+    public void structLogger40CallsWithParametrizedMessage() {
+        structLogger10CallsWithParametrizedMessage(4);
+    }
+
+    @Warmup(iterations = 5)
+    @Measurement(iterations = 5)
+    @Benchmark
+    public void structLogger50CallsWithParametrizedMessage() {
+        structLogger10CallsWithParametrizedMessage(5);
     }
 
 
     @Warmup(iterations = 5)
     @Measurement(iterations = 5)
     @Benchmark
-    public void structLogger32CallsWithParametrizedMessage() {
-        structLogger8CallsWithParametrizedMessage(4);
+    public void structLogger60CallsWithParametrizedMessage() {
+        structLogger10CallsWithParametrizedMessage(6);
     }
 
     @Warmup(iterations = 5)
     @Measurement(iterations = 5)
     @Benchmark
-    public void structLogger64CallsWithParametrizedMessage() {
-        structLogger8CallsWithParametrizedMessage(8);
+    public void structLogger70CallsWithParametrizedMessage() {
+        structLogger10CallsWithParametrizedMessage(7);
+    }
+
+    @Warmup(iterations = 5)
+    @Measurement(iterations = 5)
+    @Benchmark
+    public void structLogger80CallsWithParametrizedMessage() {
+        structLogger10CallsWithParametrizedMessage(8);
+    }
+
+    @Warmup(iterations = 5)
+    @Measurement(iterations = 5)
+    @Benchmark
+    public void structLogger90CallsWithParametrizedMessage() {
+        structLogger10CallsWithParametrizedMessage(9);
+    }
+
+    @Warmup(iterations = 5)
+    @Measurement(iterations = 5)
+    @Benchmark
+    public void structLogger100CallsWithParametrizedMessage() {
+        structLogger10CallsWithParametrizedMessage(10);
     }
 
     // structured logging with no message parametrization
@@ -121,30 +159,73 @@ public class Slf4jToFileBenchmark {
     @Warmup(iterations = 5)
     @Measurement(iterations = 5)
     @Benchmark
-    public void structuredLogging8Calls() {
-        structLogger8Calls(1);
+    public void structuredLogging10Calls() {
+        structLogger10Calls(1);
     }
 
     @Warmup(iterations = 5)
     @Measurement(iterations = 5)
     @Benchmark
-    public void structuredLogging16Calls() {
-        structLogger8Calls(2);
+    public void structuredLogging20Calls() {
+        structLogger10Calls(2);
     }
 
 
     @Warmup(iterations = 5)
     @Measurement(iterations = 5)
     @Benchmark
-    public void structuredLogging32Calls() {
-        structLogger8Calls(4);
+    public void structuredLogging30Calls() {
+        structLogger10Calls(3);
     }
 
     @Warmup(iterations = 5)
     @Measurement(iterations = 5)
     @Benchmark
-    public void structuredLogging64Calls() {
-        structLogger8Calls(8);
+    public void structuredLogging40Calls() {
+        structLogger10Calls(4);
+    }
+
+    @Warmup(iterations = 5)
+    @Measurement(iterations = 5)
+    @Benchmark
+    public void structuredLogging50Calls() {
+        structLogger10Calls(5);
+    }
+
+    @Warmup(iterations = 5)
+    @Measurement(iterations = 5)
+    @Benchmark
+    public void structuredLogging60Calls() {
+        structLogger10Calls(6);
+    }
+
+    @Warmup(iterations = 5)
+    @Measurement(iterations = 5)
+    @Benchmark
+    public void structuredLogging70Calls() {
+        structLogger10Calls(7);
+    }
+
+
+    @Warmup(iterations = 5)
+    @Measurement(iterations = 5)
+    @Benchmark
+    public void structuredLogging80Calls() {
+        structLogger10Calls(8);
+    }
+
+    @Warmup(iterations = 5)
+    @Measurement(iterations = 5)
+    @Benchmark
+    public void structuredLogging90Calls() {
+        structLogger10Calls(9);
+    }
+
+    @Warmup(iterations = 5)
+    @Measurement(iterations = 5)
+    @Benchmark
+    public void structuredLogging100Calls() {
+        structLogger10Calls(10);
     }
 
 
@@ -160,32 +241,74 @@ public class Slf4jToFileBenchmark {
     @Warmup(iterations = 5)
     @Measurement(iterations = 5)
     @Benchmark
-    public void notStructuredLogging8Calls() {
-        slf4jLog8Calls(1);
+    public void notStructuredLogging10Calls() {
+        slf4jLog10Calls(1);
     }
 
     @Warmup(iterations = 5)
     @Measurement(iterations = 5)
     @Benchmark
-    public void notStructuredLogging16Calls() {
-        slf4jLog8Calls(2);
+    public void notStructuredLogging20Calls() {
+        slf4jLog10Calls(2);
     }
 
     @Warmup(iterations = 5)
     @Measurement(iterations = 5)
     @Benchmark
-    public void notStructuredLogging32Calls() {
-        slf4jLog8Calls(4);
+    public void notStructuredLogging30Calls() {
+        slf4jLog10Calls(3);
     }
 
     @Warmup(iterations = 5)
     @Measurement(iterations = 5)
     @Benchmark
-    public void notStructuredLogging64Calls() {
-        slf4jLog8Calls(8);
+    public void notStructuredLogging40Calls() {
+        slf4jLog10Calls(4);
     }
 
-    private void structLogger8CallsWithParametrizedMessage(int number) {
+    @Warmup(iterations = 5)
+    @Measurement(iterations = 5)
+    @Benchmark
+    public void notStructuredLogging50Calls() {
+        slf4jLog10Calls(5);
+    }
+
+    @Warmup(iterations = 5)
+    @Measurement(iterations = 5)
+    @Benchmark
+    public void notStructuredLogging60Calls() {
+        slf4jLog10Calls(6);
+    }
+
+    @Warmup(iterations = 5)
+    @Measurement(iterations = 5)
+    @Benchmark
+    public void notStructuredLogging70Calls() {
+        slf4jLog10Calls(7);
+    }
+
+    @Warmup(iterations = 5)
+    @Measurement(iterations = 5)
+    @Benchmark
+    public void notStructuredLogging80Calls() {
+        slf4jLog10Calls(8);
+    }
+
+    @Warmup(iterations = 5)
+    @Measurement(iterations = 5)
+    @Benchmark
+    public void notStructuredLogging90Calls() {
+        slf4jLog10Calls(9);
+    }
+
+    @Warmup(iterations = 5)
+    @Measurement(iterations = 5)
+    @Benchmark
+    public void notStructuredLogging100Calls() {
+        slf4jLog10Calls(10);
+    }
+
+    private void structLogger10CallsWithParametrizedMessage(int number) {
         for(int i = 0; i < number; i++) {
             structLogger.info("Event with double={} and boolean={}")
                     .varDouble(1.2)
@@ -227,10 +350,23 @@ public class Slf4jToFileBenchmark {
 
             structLogger.error("Event with no parameters")
                     .log();
+
+
+            structLogger.info("Event with string={} and string={}")
+                    .varString("string value 1")
+                    .varString("string value 2")
+                    .log();
+
+            structLogger.info("Event with string={} and string={} and long={} and boolean={}")
+                    .varString("string value 1")
+                    .varString("string value 2")
+                    .varLong(1L)
+                    .varBoolean(true)
+                    .log();
         }
     }
 
-    private void structLogger8Calls(int number) {
+    private void structLogger10Calls(int number) {
         for(int i = 0; i < number; i++) {
             structLoggerNoMessageParametrization.info("Event with double and boolean")
                     .varDouble(1.2)
@@ -272,10 +408,22 @@ public class Slf4jToFileBenchmark {
 
             structLoggerNoMessageParametrization.error("Event with no parameters")
                     .log();
+
+            structLoggerNoMessageParametrization.info("Event with string and string")
+                    .varString("string value 1")
+                    .varString("string value 2")
+                    .log();
+
+            structLoggerNoMessageParametrization.info("Event with string and string and long and boolean value")
+                    .varString("string value 1")
+                    .varString("string value 2")
+                    .varLong(1L)
+                    .varBoolean(true)
+                    .log();
         }
     }
 
-    private void slf4jLog8Calls(int number) {
+    private void slf4jLog10Calls(int number) {
         for(int i = 0; i < number; i++) {
             logger.info("log string={} and boolean={}", 1.2, false);
 
@@ -292,6 +440,10 @@ public class Slf4jToFileBenchmark {
             logger.warn("log boolean={} and boolean={} and integer={}", true, false, 1);
 
             logger.error("log with no parameters");
+
+            logger.info("log with string={} and string={}", "string value 1", "string value 2");
+
+            logger.info("log with string={} and string={} and long={} and boolean={}", "string value 1", "string value 2", 1L, true);
         }
     }
 }
