bce6f7c17f2450f8538c4c4471eb61107feac833
Tantalor93 <Obenky@gmail.com>
2018-02-15 10:24:19 +0100

    slf4jloggingcallback test

---
+++ structlogger-tests/src/test/java/cz/muni/Slf4jLoggingCallbackTest.java
@@ -0,0 +1,69 @@
+package cz.muni;
+
+import cz.muni.fi.EventLogger;
+import cz.muni.fi.Slf4jLoggingCallback;
+import cz.muni.fi.annotation.LoggerContext;
+import org.junit.Before;
+import org.junit.Test;
+import org.slf4j.Logger;
+
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+
+public class Slf4jLoggingCallbackTest {
+
+    private Logger logger = mock(Logger.class);
+
+    @LoggerContext(context = TestContext.class)
+    private EventLogger<TestContext> structLogger;
+
+    @Before
+    public void setUp() throws Exception {
+        logger = mock(Logger.class);
+        structLogger = new EventLogger<>(
+                new Slf4jLoggingCallback(logger)
+        );
+    }
+
+    @Test
+    public void testLoggerIsCalled() throws Exception {
+        structLogger
+                .info("test logger is called")
+                .varInt(1)
+                .varString("ahoj")
+                .log();
+
+        structLogger
+                .warn("test logger is called")
+                .varInt(1)
+                .varString("ahoj")
+                .log();
+        
+        structLogger
+                .error("test logger is called")
+                .varInt(1)
+                .varString("ahoj")
+                .log();
+
+        structLogger
+                .debug("test logger is called")
+                .varInt(1)
+                .varString("ahoj")
+                .log();
+
+        structLogger
+                .trace("test logger is called")
+                .varInt(1)
+                .varString("ahoj")
+                .log();
+
+        structLogger
+                .audit("test logger is called")
+                .varInt(1)
+                .varString("ahoj")
+                .log();
+
+        verify(logger, times(6));
+    }
+}
