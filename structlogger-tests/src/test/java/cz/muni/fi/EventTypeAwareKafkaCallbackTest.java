58027e348f611766e54d26f31d4922b251081038
Tantalor93 <Obenky@gmail.com>
2018-02-18 14:58:58 +0100

    add tests for kafka callback

---
+++ structlogger-tests/src/test/java/cz/muni/fi/EventTypeAwareKafkaCallbackTest.java
@@ -0,0 +1,60 @@
+package cz.muni.fi;
+
+import cz.muni.fi.annotation.LoggerContext;
+import cz.muni.fi.kafka.EventTypeAwareKafkaCallback;
+import cz.muni.fi.kafka.LoggingEventJsonSerializer;
+import org.apache.kafka.clients.producer.MockProducer;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.serialization.LongSerializer;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+
+public class EventTypeAwareKafkaCallbackTest {
+
+    private MockProducer mockProducer;
+
+    private EventTypeAwareKafkaCallback kafkaCallback;
+
+    @LoggerContext(context = TestContext.class)
+    private EventLogger<TestContext> testLogger;
+
+    @Before
+    public void setUp() {
+        mockProducer = new MockProducer<>(
+                true,
+                new LongSerializer(),
+                new LoggingEventJsonSerializer()
+        );
+
+        kafkaCallback = new EventTypeAwareKafkaCallback(mockProducer);
+
+        testLogger = new EventLogger<>(kafkaCallback);
+    }
+
+    @Test
+    public void testEventIsSerializedAndSentUsingProducer() throws Exception {
+        testLogger.info("Event sent via kafka")
+                .varInt(10)
+                .varString("string value")
+                .log("KafkaEvent1");
+
+        testLogger.info("Event sent via kafka")
+                .varInt(10)
+                .varString("string value")
+                .log("KafkaEvent2");
+
+        assertThat(mockProducer.history().size(), is(2));
+
+        final ProducerRecord e0 = (ProducerRecord) mockProducer.history().get(0);
+        final ProducerRecord e1 = (ProducerRecord) mockProducer.history().get(1);
+
+        final LoggingEvent loggingEventE0 = (LoggingEvent) e0.value();
+        final LoggingEvent loggingEventE1 = (LoggingEvent) e1.value();
+
+        assertThat(loggingEventE0.getType(), is("KafkaEvent1"));
+        assertThat(loggingEventE1.getType(), is("KafkaEvent2"));
+    }
+}
