01a4fe250a4781d3df6cb9da0ca089d5b4455fe5
Tantalor93 <Obenky@gmail.com>
2018-03-09 20:20:11 +0100

    fix logstash benchmark

--- structlogger-benchmark/src/main/java/com/github/structlogging/Slf4jToFileBenchmark.java
+++ structlogger-benchmark/src/main/java/com/github/structlogging/Slf4jToFileBenchmark.java
@@ -339,7 +339,7 @@ public class Slf4jToFileBenchmark {
     @Measurement(iterations = 5)
     @Benchmark
     public void logstashStructuredParametrizedMessageLogging1Call() {
-        logger.info("Event with double={} and boolean={}", 1.2, false);
+        loggerLogstashParametrizedMessage.info("Event with double={} and boolean={}", 1.2, false);
     }
 
     @Warmup(iterations = 5)
@@ -418,7 +418,7 @@ public class Slf4jToFileBenchmark {
     @Measurement(iterations = 5)
     @Benchmark
     public void logstashStructuredLogging1Calls() {
-        logger.info("Event with double and boolean", 1.2, false);
+        loggerLogstash.info("Event with double and boolean", 1.2, false);
     }
 
     @Warmup(iterations = 5)
